eff4b9c5e18ed317c449c5ee52771591599ad64d
Sparta16 <adenslep@mail.ru>
2024-05-13 19:04:08 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module club
+
+go 1.22.1
+
+require github.com/Sparta16/club v0.0.0-20240513140936-c28a46b72124
---
+++ src/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Sparta16/club"
+)
+
+func main() {
+	fmt.Println(app.Club())
+}
